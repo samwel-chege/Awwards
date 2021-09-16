488c703bb8af1125140043852764acc7b4d81e70
samwel-chege <samwelchegeh09@gmail.com>
2021-09-17 01:18:19 +0300

    add authentication paths

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include('awwards.urls')),
+    path('logout/',views.LogoutView.as_view()),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
