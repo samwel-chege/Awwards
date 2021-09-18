c00a8aa28eb23b0e84733d0ff514a5af4f3060e3
samwel-chege <samwelchegeh09@gmail.com>
2021-09-18 19:00:25 +0300

    query and pass in project and profile contexts

--- awwards/views.py
+++ awwards/views.py
@@ -1,5 +1,9 @@
+from awwards.models import Profile
 from django.shortcuts import render
-
+from awwards.models import Profile, Project
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    projects = Project.display_projects()
+    profile = Profile.objects.all()
+
+    return render(request,'index.html',{"projects":projects,"profile":profile})
