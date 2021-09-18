0bfae300090f8dec11220d49af9869746066734b
samwel-chege <samwelchegeh09@gmail.com>
2021-09-18 19:02:05 +0300

    change max-length of emailfield

--- awwards/models.py
+++ awwards/models.py
@@ -1,14 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.fields import TextField
+from django.dispatch import receiver
+from django.db.models.signals  import post_save
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
     photo = models.ImageField(upload_to='photos')
     bio = models.TextField(max_length=100)
-    contact = models.EmailField(max_length=20)
+    contact = models.EmailField(max_length=50)
     link = models.URLField()
+
+    @receiver(post_save,sender = User)
+    def update_user_profile(sender,instance,created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        try:
+            instance.profile.save()
+        except AttributeError:
+            pass  
+
+
    
 class Project(models.Model):
     title = models.TextField(max_length=10)
@@ -17,6 +30,28 @@ class Project(models.Model):
     link  = models.URLField()
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def display_projects(cls):
+        return cls.objects.all()
+
+    @classmethod
+    def get_user_projects(cls,profile):
+        return cls.objects.filter(profile=profile)  
+
+    class Meta:
+        ordering = ['-create_date']                  
+
+
 
 
 
