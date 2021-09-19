c47e9d8f198660f94ffb2dd0451c9a7417db03e3
samwel-chege <samwelchegeh09@gmail.com>
2021-09-19 15:50:13 +0300

    create a class profileserializer

--- awwards/serializer.py
+++ awwards/serializer.py
@@ -5,3 +5,8 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ('title','image','description','link','profile','create_date')
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('user','photo','bio','contact','link')
