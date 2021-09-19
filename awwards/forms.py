9f7a3840688032fc25e4b4006a553afb1704f522
samwel-chege <samwelchegeh09@gmail.com>
2021-09-19 18:12:59 +0300

    create a form to upload projects

--- awwards/forms.py
+++ awwards/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Project,Profile
+from django.forms import ModelForm
 
 class ProjectForm(forms.Form):
     class Meta:
@@ -10,4 +11,9 @@ class ProfileForm(forms.Form):
     class Meta:
         model = Profile
         fields = ('user','photo','bio','contact','link')
+
+class UploadProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ('title','image','description','link','profile','create_date')     
         
