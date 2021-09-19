c8354384e5a1167d2608fbcac4cdd0dbe5b0cd90
samwel-chege <samwelchegeh09@gmail.com>
2021-09-19 15:46:58 +0300

    create a profileform

--- awwards/forms.py
+++ awwards/forms.py
@@ -5,3 +5,9 @@ class ProjectForm(forms.Form):
     class Meta:
         model = Project
         fields = ('title','image','description','link','profile','create_date')
+
+class ProfileForm(forms.Form):
+    class Meta:
+        model = Profile
+        fields = ('user','photo','bio','contact','link')
+        
