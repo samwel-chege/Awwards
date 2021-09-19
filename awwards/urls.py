44f6fbf93f9cdd0dcbad4b0d113a8024107bcd8f
samwel-chege <samwelchegeh09@gmail.com>
2021-09-19 14:31:20 +0300

    comment out useless path

--- awwards/urls.py
+++ awwards/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
+import rest_framework
 from .import views
 from django.conf import settings 
 from django.conf.urls.static import static
+# from rest_framework import routers
+# from .api import ProjectViewSet
+
+#create an instance of the router
+# router = routers.DefaultRouter()
+# router.register('/api/awwards', ProjectViewSet,'awwards')
 
 
 urlpatterns = [
     path('', views.home,name='home'),
+ 
 ]
 
 if settings.DEBUG:
