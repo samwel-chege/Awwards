64a782f413c74f0403ef5b2b777ce43d2bb74cce
samwel-chege <samwelchegeh09@gmail.com>
2021-09-19 23:31:33 +0300

    edit the template path

--- awwards/views.py
+++ awwards/views.py
@@ -1,13 +1,14 @@
 
 from awwards.models import Profile
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from awwards.models import Profile, Project
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Project
 from .serializer import ProjectSerializer,ProfileSerializer
 from awwards import serializer
-from .forms import ProjectForm
+from .forms import ProjectForm,UploadProjectForm
+from django.http.response import Http404, HttpResponseRedirect
 # Create your views here.
 def home(request):
     projects = Project.display_projects()
@@ -39,7 +40,26 @@ class ProfileList(APIView):
     def get(self,request,format=None):
         all_profiles = Profile.objects.all()
         serializers = ProfileSerializer(all_profiles,many=True)
-        return Response(serializers.data)        
+        return Response(serializers.data)    
+
+
+def uploadproject(request):
+    current_user = request.user
+    try:
+        profile = Profile.objects.get(user = current_user)
+    except Profile.DoesNotExist:
+        raise Http404()
+    if request.method == "POST":
+        form = UploadProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.profile = profile
+            image.save()
+        return redirect('home')
+    else:
+        form = UploadProjectForm()
+    return render(request, 'project/upload_project.html', {"form": form})                
+
 
 
 
