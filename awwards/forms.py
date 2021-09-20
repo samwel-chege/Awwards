6e199bec13f9effdff4e1cd928e4a97c67edd735
samwel-chege <samwelchegeh09@gmail.com>
2021-09-20 17:58:45 +0300

    create rate form

--- awwards/forms.py
+++ awwards/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Project,Profile
+from .models import Project,Profile,Rate
 from django.forms import ModelForm
 
 class ProjectForm(forms.Form):
@@ -15,5 +15,10 @@ class ProfileForm(forms.Form):
 class UploadProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ('title','image','description','link',)     
+        fields = ('title','image','description','link',)  
+
+class RateForm(ModelForm):
+    class Meta:
+        model = Rate  
+        fields = ('design','usability','content')         
         
