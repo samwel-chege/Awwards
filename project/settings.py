5b2dfdc5db9fdc42e0fdef165514e4501d957baf
samwel-chege <samwelchegeh09@gmail.com>
2021-09-23 01:08:27 +0300

    make changes

--- project/settings.py
+++ project/settings.py
@@ -36,7 +36,7 @@ if config('MODE')=="dev":
 else:
    DATABASES = {
        'default': dj_database_url.config(
-           default=config('DATABASE_URL')
+           default=config('DTB_URL')
        )
    }
 
@@ -53,13 +53,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)qa#k@bj*19lpkv^!pcgf65wltlendosvrkxdslyy&upklr^35'
+# SECRET_KEY = 'django-insecure-)qa#k@bj*19lpkv^!pcgf65wltlendosvrkxdslyy&upklr^35'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
