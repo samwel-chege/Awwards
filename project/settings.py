66fc97918e90e2da8aa7aacf4abfeff12c7de957
samwel-chege <samwelchegeh09@gmail.com>
2021-09-23 00:59:12 +0300

    make changes in deployment settings

--- project/settings.py
+++ project/settings.py
@@ -18,7 +18,7 @@ from decouple import config,Csv
 
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = os.environ.get('DEBUG', False)
 # development
 if config('MODE')=="dev":
    DATABASES = {
